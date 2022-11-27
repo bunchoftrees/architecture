2006e9c5225b53fcb9a241fb5725c49c02d45a5c
Forrest JB Hartley <forrest@forrestbear.org>
2022-11-27 10:53:35 -0500

    test push

--- Cypher/Program.cs
+++ Cypher/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Linq;
-    namespace Cypher
+
+namespace Cypher
+{
+    internal class Program
     {
-        internal class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
             char[] alphabet = Enumerable.Range('a', 26).Select(x => (char)x).ToArray();
 
-            foreach(char c in alphabet)
-                {
+            foreach (char c in alphabet)
+            {
                 Console.WriteLine(c);
-            }
             }
         }
     }
+}
