abc549ed1b54f9094dfc45c062503be2894374c8
Forrest Hartley <forrest@forrestbear.org>
2022-11-27 02:28:23 -0500

    added unformatted start to cypher app

---
+++ Cypher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+    namespace Cypher
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World");
+            char[] alphabet = Enumerable.Range('a', 26).Select(x => (char)x).ToArray();
+
+            foreach(char c in alphabet)
+                {
+                Console.WriteLine(c);
+            }
+            }
+        }
+    }
