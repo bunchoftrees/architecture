a930588974b105be48236ea470d17bbcd3ba58bc
Forrest Hartley <forrest@forrestbear.org>
2022-11-28 10:26:16 -0500

    updated logic to allow for hobbies to be blank

--- DatingConsole/Profile.cs
+++ DatingConsole/Profile.cs
@@ -2,47 +2,46 @@ using System;
 
 namespace DatingConsole
 {
-    class Profile
-    {
-        // Fields
-        private string name;
-        private int age;
-        private string city;
-        private string country;
-        private string pronouns;
-        private string[] hobbies;
+	class Profile
+	{
+		// Fields
+		private string name;
+		private int age;
+		private string city;
+		private string country;
+		private string pronouns;
+		private string[] hobbies;
 
-        // Constructors
-        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
-        {
-            this.name = name;
-            this.age = age;
-            this.city = city;
-            this.country = country;
-            this.pronouns = pronouns;
-            this.hobbies = new string[0];
-        }
+		// Constructors
+		public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+		{
+			this.name = name;
+			this.age = age;
+			this.city = city;
+			this.country = country;
+			this.pronouns = pronouns;
+			this.hobbies = new string[0];
+		}
 
-        // Methods
-        public string ViewProfile()
-        {
-            string userProfile = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}\n";
+		// Methods
+		public string ViewProfile()
+		{
+			string userProfile = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}\n";
 
-            if (hobbies.Length > 0)
-            {
-                userProfile += "Hobbies:\n";
-                foreach (string hobby in hobbies)
-                {
-                    
-                    userProfile += $"* {hobby}\n";
-                }
-            }
-            return userProfile;
-        }
+			if (hobbies.Length > 0)
+			{
+				userProfile += "Hobbies:\n";
+				foreach (string hobby in hobbies)
+				{
+					userProfile += $"* {hobby}\n";
+				}
+			}
+			return userProfile;
+		}
 
-        public void SetHobbies(string[] hobbies)
-        {
-            this.hobbies = hobbies;
-        }
-    }
+		public void SetHobbies(string[] hobbies)
+		{
+			this.hobbies = hobbies;
+		}
+	}
 }
