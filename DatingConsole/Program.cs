83b356e83a4583113e8464654f43a254ff233b98
Forrest Hartley <forrest@forrestbear.org>
2022-11-28 09:49:17 -0500

    updated formatting

--- DatingConsole/Program.cs
+++ DatingConsole/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-
-namespace DatingConsole
+﻿namespace DatingConsole
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Profile sam = new Profile("Forrest Hartley", 35, "Boston", "US", "he/him");
-      Console.WriteLine(sam.ViewProfile());
-      sam.SetHobbies(new string[] {"reading", "writing computer programs", "watching star trek"});
-      System.Console.WriteLine(sam.ViewProfile());
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Forrest Hartley", 35, "Boston", "US", "he/him");
+            Console.WriteLine(sam.ViewProfile());
+            sam.SetHobbies(new string[] { "reading", "writing computer programs", "watching star trek" });
+            Console.WriteLine(sam.ViewProfile());
+        }
+
     }
-  }
 }
